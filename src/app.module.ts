7db3bf6d7fadaac91c1bbe94b5aa10bf52c69d6e
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-01-05 21:21:26 -0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
+import { FileModule } from './file/file.module';
+import { InteractionModule } from './interaction/interaction.module';
+import { PageModule } from './page/page.module';
 import { PublicationModule } from './publication/publication.module';
 import { TemplateModule } from './template/template.module';
-import { FileModule } from './file/file.module';
 import { UserModule } from './user/user.module';
-import { PageModule } from './page/page.module';
 import { WorkspaceModule } from './workspace/workspace.module';
-import { InteractionModule } from './interaction/interaction.module';
 
 @Module({
-  imports: [PublicationModule, TemplateModule, FileModule, UserModule, PageModule, WorkspaceModule, InteractionModule],
+  imports: [
+    PublicationModule,
+    TemplateModule,
+    FileModule,
+    UserModule,
+    PageModule,
+    WorkspaceModule,
+    InteractionModule,
+  ],
   controllers: [],
   providers: [],
 })
