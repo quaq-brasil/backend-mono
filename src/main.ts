7b72a45131c2c080e151d8075630d9ff0018dcdd
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-01-25 14:10:57 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
+  app.enableCors({
+    origin: ['http://localhost:3000/'],
+  });
 
   await app.listen(3000);
 }
