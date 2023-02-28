41a215aaa4c7706c99f8e6b45a78b80114493ce8
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-02-28 19:45:59 -0300

    feat: members

--- src/app/workspace/workspace.controller.ts
+++ src/app/workspace/workspace.controller.ts
@@ -1,15 +1,7 @@
-import {
-	Body,
-	Controller,
-	Delete,
-	Get,
-	Param,
-	Post,
-	Put,
-} from '@nestjs/common';
-import { CreateWorkspaceDto } from './dto/create-workspace.dto';
-import { UpdateWorkspaceDto } from './dto/update-workspace.dto';
-import { WorkspaceService } from './workspace.service';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import { CreateWorkspaceDto } from './dto/create-workspace.dto'
+import { UpdateWorkspaceDto } from './dto/update-workspace.dto'
+import { WorkspaceService } from './workspace.service'
 
 @Controller('api/v1/workspaces')
 export class WorkspaceController {
@@ -17,17 +9,33 @@ export class WorkspaceController {
 
 	@Post()
 	create(@Body() createWorkspaceDto: CreateWorkspaceDto) {
-		return this.workspaceService.create(createWorkspaceDto);
+		return this.workspaceService.create(createWorkspaceDto)
 	}
 
 	@Get(':id')
 	findOne(@Param('id') id: string) {
-		return this.workspaceService.findOne(id);
+		return this.workspaceService.findOne(id)
 	}
 
 	@Get('/user/:id')
 	findManyByUserId(@Param('id') id: string) {
-		return this.workspaceService.findAllByUserId(id);
+		return this.workspaceService.findAllByUserId(id)
+	}
+
+	@Post('/members/add/:id')
+	addMember(
+		@Param('id') id: string,
+		@Body() data: { email: string; id?: string },
+	) {
+		return this.workspaceService.addWorkspaceMember(id, data.email)
+	}
+
+	@Post('/members/remove/:id')
+	removeMember(
+		@Param('id') id: string,
+		@Body() data: { email: string; user_id?: string },
+	) {
+		return this.workspaceService.removeWorkspaceMember(id, data.user_id)
 	}
 
 	@Post('/generate_unique_slug')
@@ -35,17 +43,17 @@ export class WorkspaceController {
 		return this.workspaceService.generateUniqueSlugByWorkspaceName(
 			data.name,
 			data.id,
-		);
+		)
 	}
 
 	@Get('/slug/:slug')
 	findManyBySlug(@Param('slug') slug: string) {
-		return this.workspaceService.findOneBySlug(slug);
+		return this.workspaceService.findOneBySlug(slug)
 	}
 
 	@Get('/page/slug/:slug')
 	findOneByPageSlug(@Param('slug') slug: string) {
-		return this.workspaceService.findOneByPageSlug(slug);
+		return this.workspaceService.findOneByPageSlug(slug)
 	}
 
 	@Put(':id')
@@ -53,11 +61,11 @@ export class WorkspaceController {
 		@Param('id') id: string,
 		@Body() updateWorkspaceDto: UpdateWorkspaceDto,
 	) {
-		return this.workspaceService.update(id, updateWorkspaceDto);
+		return this.workspaceService.update(id, updateWorkspaceDto)
 	}
 
 	@Delete(':id')
 	remove(@Param('id') id: string) {
-		return this.workspaceService.remove(id);
+		return this.workspaceService.remove(id)
 	}
 }
--- src/app/workspace/workspace.service.ts
+++ src/app/workspace/workspace.service.ts
@@ -1,38 +1,38 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
-import { CreateWorkspaceDto } from './dto/create-workspace.dto';
-import { UpdateWorkspaceDto } from './dto/update-workspace.dto';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { CreateWorkspaceDto } from './dto/create-workspace.dto'
+import { UpdateWorkspaceDto } from './dto/update-workspace.dto'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const getSlug = require('speakingurl');
+const getSlug = require('speakingurl')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const uniqueSlug = require('unique-slug');
+const uniqueSlug = require('unique-slug')
 
 @Injectable()
 export class WorkspaceService {
-	private readonly logger = new Logger(WorkspaceService.name);
-	private readonly MAX_ATTEMPTS = 10;
-	private instance: WorkspaceService;
+	private readonly logger = new Logger(WorkspaceService.name)
+	private readonly MAX_ATTEMPTS = 10
+	private instance: WorkspaceService
 
 	constructor(private prismaService: PrismaService) {}
 
 	getInstance() {
 		if (!this.instance) {
-			this.instance = new WorkspaceService(this.prismaService);
+			this.instance = new WorkspaceService(this.prismaService)
 		}
 
-		return this.instance;
+		return this.instance
 	}
 
 	async create(createWorkspaceDto: CreateWorkspaceDto) {
 		if (createWorkspaceDto.slug) {
 			createWorkspaceDto.slug = await this.generateUniqueSlugByWorkspaceName(
 				createWorkspaceDto.slug,
-			);
+			)
 		}
 
 		return await this.prismaService.workspace.create({
 			data: createWorkspaceDto,
-		});
+		})
 	}
 
 	findOne(id: string) {
@@ -40,7 +40,7 @@ export class WorkspaceService {
 			where: {
 				id,
 			},
-		});
+		})
 	}
 
 	async findOneBySlug(slug: string) {
@@ -48,7 +48,7 @@ export class WorkspaceService {
 			where: {
 				slug,
 			},
-		});
+		})
 	}
 
 	async findAllByUserId(user_id: string) {
@@ -59,7 +59,7 @@ export class WorkspaceService {
 			include: {
 				Page: true,
 			},
-		});
+		})
 	}
 
 	async findOneByPageSlug(page_slug: string) {
@@ -70,16 +70,16 @@ export class WorkspaceService {
 			include: {
 				Workspace: true,
 			},
-		});
+		})
 
-		return page.Workspace;
+		return page.Workspace
 	}
 
 	async update(id: string, updateWorkspaceDto: UpdateWorkspaceDto) {
 		if (updateWorkspaceDto.slug) {
 			updateWorkspaceDto.slug = await this.generateUniqueSlugByWorkspaceName(
 				updateWorkspaceDto.slug,
-			);
+			)
 		}
 
 		return await this.prismaService.workspace.update({
@@ -87,7 +87,7 @@ export class WorkspaceService {
 				id,
 			},
 			data: updateWorkspaceDto,
-		});
+		})
 	}
 
 	remove(id: string) {
@@ -95,27 +95,104 @@ export class WorkspaceService {
 			where: {
 				id,
 			},
-		});
+		})
+	}
+
+	async addWorkspaceMember(id: string, email: string) {
+		const user = await this.prismaService.user.findUnique({
+			where: {
+				email,
+			},
+		})
+
+		if (!user) {
+			throw new BadRequestException({ message: 'user not found' })
+		}
+
+		const workspace = await this.prismaService.workspace.findUnique({
+			where: {
+				id,
+			},
+		})
+
+		if (!workspace) {
+			throw new BadRequestException({ message: 'workspace not found' })
+		}
+
+		return await this.prismaService.workspace.update({
+			where: {
+				id,
+			},
+			data: {
+				members: [
+					...workspace.members,
+					{
+						id: user.id,
+						email: user.email,
+						avatar_url: user.avatar_url,
+						name: user.name,
+					},
+				],
+			},
+		})
+	}
+
+	async removeWorkspaceMember(id: string, user_id: string) {
+		console.log('user_id', user_id)
+		const user = await this.prismaService.user.findUnique({
+			where: {
+				id: user_id,
+			},
+		})
+
+		if (!user) {
+			throw new BadRequestException({ message: 'user not found' })
+		}
+
+		const workspace = await this.prismaService.workspace.findUnique({
+			where: {
+				id,
+			},
+		})
+
+		if (!workspace) {
+			throw new BadRequestException({ message: 'workspace not found' })
+		}
+
+		const newMembers = workspace.members.filter(
+			(member: any) => member?.id !== user_id,
+		)
+
+		console.log('newMembers', newMembers)
+
+		return await this.prismaService.workspace.update({
+			where: {
+				id,
+			},
+			data: {
+				members: newMembers,
+			},
+		})
 	}
 
 	async generateUniqueSlugByWorkspaceName(name: string, id?: string) {
-		const slug = getSlug(name);
+		const slug = getSlug(name)
 
-		let uniqSlug = slug;
+		let uniqSlug = slug
 
-		let attempts = 0;
+		let attempts = 0
 
 		while (await this.isSlugTaken(uniqSlug, id)) {
-			attempts++;
+			attempts++
 			if (attempts >= this.MAX_ATTEMPTS) {
-				throw new BadRequestException('Could not generate unique slug');
+				throw new BadRequestException('Could not generate unique slug')
 			}
 
-			const randomSlug: string = uniqueSlug();
-			uniqSlug = `${slug}-${randomSlug.slice(0, 3)}`;
+			const randomSlug: string = uniqueSlug()
+			uniqSlug = `${slug}-${randomSlug.slice(0, 3)}`
 		}
 
-		return uniqSlug;
+		return uniqSlug
 	}
 
 	async isSlugTaken(slug: string, id?: string) {
@@ -123,16 +200,16 @@ export class WorkspaceService {
 			try {
 				const workspace = await this.prismaService.workspace.findUnique({
 					where: { slug },
-				});
+				})
 
 				if (!workspace) {
-					return false;
+					return false
 				}
 
-				return workspace.id !== id;
+				return workspace.id !== id
 			} catch (error) {
-				this.logger.error(`Error checking if slug is taken: ${error.message}`);
-				throw new BadRequestException({ message: error.message });
+				this.logger.error(`Error checking if slug is taken: ${error.message}`)
+				throw new BadRequestException({ message: error.message })
 			}
 		}
 
@@ -140,10 +217,10 @@ export class WorkspaceService {
 			return (
 				(await this.prismaService.workspace.findMany({ where: { slug } }))
 					.length > 0
-			);
+			)
 		} catch (error) {
-			this.logger.error(`Error checking if slug is taken: ${error.message}`);
-			throw new BadRequestException({ message: error.message });
+			this.logger.error(`Error checking if slug is taken: ${error.message}`)
+			throw new BadRequestException({ message: error.message })
 		}
 	}
 }
