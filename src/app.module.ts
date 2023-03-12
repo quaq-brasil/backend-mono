b5dfbc74b40064917ea0387850a8b796d6858327
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-03-12 15:17:23 -0300

    fix: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,33 @@
-import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { AbilityModule } from './app/ability/ability.module'
-import { AuthModule } from './app/auth/auth.module'
-import { FileModule } from './app/file/file.module'
-import { InteractionModule } from './app/interaction/interaction.module'
-import { PageModule } from './app/page/page.module'
-import { PublicationModule } from './app/publication/publication.module'
-import { S3FileUploadModule } from './app/s3-upload/s3-upload.module'
-import { TemplateModule } from './app/template/template.module'
-import { UserModule } from './app/user/user.module'
-import { VariablesModule } from './app/variables/variables.module'
-import { WorkspaceModule } from './app/workspace/workspace.module'
+import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
+import { AbilityModule } from "./app/ability/ability.module"
+import { AuthModule } from "./app/auth/auth.module"
+import { FileModule } from "./app/file/file.module"
+import { InteractionModule } from "./app/interaction/interaction.module"
+import { PageModule } from "./app/page/page.module"
+import { PublicationModule } from "./app/publication/publication.module"
+import { S3FileUploadModule } from "./app/s3-upload/s3-upload.module"
+import { TemplateModule } from "./app/template/template.module"
+import { UserModule } from "./app/user/user.module"
+import { VariablesModule } from "./app/variables/variables.module"
+import { WorkspaceModule } from "./app/workspace/workspace.module"
 
 @Module({
-	imports: [
-		ConfigModule.forRoot(),
-		TemplateModule,
-		PublicationModule,
-		UserModule,
-		FileModule,
-		PageModule,
-		WorkspaceModule,
-		InteractionModule,
-		AuthModule,
-		VariablesModule,
-		S3FileUploadModule,
-		AbilityModule
-	],
-	controllers: [],
-	providers: []
+  imports: [
+    ConfigModule.forRoot(),
+    TemplateModule,
+    PublicationModule,
+    UserModule,
+    FileModule,
+    PageModule,
+    WorkspaceModule,
+    InteractionModule,
+    AuthModule,
+    VariablesModule,
+    S3FileUploadModule,
+    AbilityModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/app/ability/abilities.decorator.ts
+++ src/app/ability/abilities.decorator.ts
@@ -1,13 +1,13 @@
-import { SetMetadata } from '@nestjs/common'
-import { Action, defineAbilityProps } from './ability.factory'
+import { SetMetadata } from "@nestjs/common";
+import { Action, defineAbilityProps } from "./ability.factory";
 
-export const CHECK_ABILITY = 'check_ability'
+export const CHECK_ABILITY = "check_ability";
 
 export interface RequiredRule {
-	action: Action
-	subject: string
-	props?: defineAbilityProps
+	action: Action;
+	subject: string;
+	props?: defineAbilityProps;
 }
 
 export const CheckAbilities = (...requirements: RequiredRule[]) =>
-	SetMetadata(CHECK_ABILITY, requirements)
+	SetMetadata(CHECK_ABILITY, requirements);
