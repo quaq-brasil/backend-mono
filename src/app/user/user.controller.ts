57c192f0adc5ce5828761a340569aa4abd53c4ec
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-04-20 14:38:37 -0300

    fix: sign up

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,44 +1,49 @@
 import {
-	Body,
-	Controller,
-	Delete,
-	Get,
-	HttpCode,
-	HttpStatus,
-	Post,
-	Put,
-	Req,
-	UseGuards
-} from '@nestjs/common'
-import { JwtGuard } from '../auth/jwt.guard'
-import { UpdateUserDto } from './dto/update-user.dto'
-import { UserService } from './user.service'
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Req,
+  UseGuards,
+} from "@nestjs/common"
+import { JwtGuard } from "../auth/jwt.guard"
+import { UpdateUserDto } from "./dto/update-user.dto"
+import { UserService } from "./user.service"
 
-@Controller('api/v1/users')
+@Controller("api/v1/users")
 export class UserController {
-	constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-	@Post()
-	async create() {
-		return await this.userService.create()
-	}
+  @Post()
+  async create() {
+    return await this.userService.create()
+  }
 
-	@UseGuards(JwtGuard)
-	@Get()
-	async findOne(@Req() req) {
-		return await this.userService.findOne(req.user.sub)
-	}
+  @UseGuards(JwtGuard)
+  @Get()
+  async findOne(@Req() req) {
+    return await this.userService.findOne(req.user.sub)
+  }
 
-	@UseGuards(JwtGuard)
-	@Put(':user_id')
-	async update(@Req() req, @Body() updateUserDto: UpdateUserDto) {
-		return await this.userService.update(req.user.sub, updateUserDto)
-	}
+  // @UseGuards(JwtGuard)
+  @Put(":user_id")
+  async update(
+    @Param("user_id") user_id: string,
+    @Req() req,
+    @Body() updateUserDto: UpdateUserDto
+  ) {
+    return await this.userService.update(user_id, updateUserDto)
+  }
 
-	@UseGuards(JwtGuard)
-	@Delete(':user_id')
-	@HttpCode(HttpStatus.NO_CONTENT)
-	async remove(@Req() req) {
-		return await this.userService.remove(req.user.sub)
-	}
+  @UseGuards(JwtGuard)
+  @Delete(":user_id")
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Req() req) {
+    return await this.userService.remove(req.user.sub)
+  }
 }
