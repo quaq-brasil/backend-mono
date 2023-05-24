d55f3dade2837e0ac08b7d72535ae3b73fa8b860
felpst <felipe.t.v.d@gmail.com>
2023-05-24 20:09:48 -0300

    feat: add multiple file formats to file uploader

--- src/app/s3-upload/s3-upload.controller.ts
+++ src/app/s3-upload/s3-upload.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from "@nestjs/common"
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common"
 import { FileInterceptor } from "@nestjs/platform-express"
 import { FileEntity } from "@prisma/client"
 import { S3FileUploadService } from "./s3-upload.service"
@@ -8,7 +14,24 @@ export class S3FileUploadController {
   constructor(private fileUploadService: S3FileUploadService) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor("file"))
+  @UseInterceptors(
+    FileInterceptor("file", {
+      fileFilter: (req, file, cb) => {
+        const allowedMimes = [
+          "application/pdf",
+          "image/jpeg",
+          "image/png",
+          "image/svg+xml",
+        ]
+
+        if (allowedMimes.includes(file.mimetype)) {
+          cb(null, true)
+        } else {
+          cb(new BadRequestException("Invalid file type."), false)
+        }
+      },
+    })
+  )
   async uploadFile(
     @UploadedFile() file: Express.Multer.File
   ): Promise<FileEntity> {
