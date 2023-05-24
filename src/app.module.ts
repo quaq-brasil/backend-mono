c3906c8cb0717ba07644cb4d19b10aa0a27247f7
Carlos Daniel Rigoni <icarlosrigoni@gmail.com>
2023-05-24 15:48:18 -0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
-import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common"
+import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
-import rateLimit from "express-rate-limit"
 import { AbilityModule } from "./app/ability/ability.module"
 import { AuthModule } from "./app/auth/auth.module"
 import { BlockModule } from "./app/block/block.module"
@@ -9,13 +8,13 @@ import { FileModule } from "./app/file/file.module"
 import { InteractionModule } from "./app/interaction/interaction.module"
 import { PageModule } from "./app/page/page.module"
 import { PublicationModule } from "./app/publication/publication.module"
+import { RaffleModule } from "./app/raffle/raffle.module"
 import { S3FileUploadModule } from "./app/s3-upload/s3-upload.module"
 import { SitemapModule } from "./app/sitemap/sitemap.module"
 import { TemplateModule } from "./app/template/template.module"
 import { UserModule } from "./app/user/user.module"
 import { VariablesModule } from "./app/variables/variables.module"
 import { WorkspaceModule } from "./app/workspace/workspace.module"
-import { RaffleModule } from './app/raffle/raffle.module';
 
 @Module({
   imports: [
@@ -40,14 +39,14 @@ import { RaffleModule } from './app/raffle/raffle.module';
   providers: [],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(
-        rateLimit({
-          windowMs: 15 * 60 * 1000, // 15 minutes
-          max: 200, // limit each IP to 100 requests per windowMs
-        })
-      )
-      .forRoutes({ path: "*", method: RequestMethod.ALL })
-  }
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer
+  //     .apply(
+  //       rateLimit({
+  //         windowMs: 15 * 60 * 1000, // 15 minutes
+  //         max: 200, // limit each IP to 100 requests per windowMs
+  //       })
+  //     )
+  //     .forRoutes({ path: "*", method: RequestMethod.ALL })
+  // }
 }
